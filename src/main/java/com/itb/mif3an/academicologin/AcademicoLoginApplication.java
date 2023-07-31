5119b7e6f9e4936de4f344914df293a38333bf5d
Gessivan Junior <priceddread13@gmail.com>
2023-07-31 20:38:10 -0300

    inicio controle de versao

---
+++ src/main/java/com/itb/mif3an/academicologin/AcademicoLoginApplication.java
@@ -0,0 +1,13 @@
+package com.itb.mif3an.academicologin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademicoLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademicoLoginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itb/mif3an/academicologin/AcademicoLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itb.mif3an.academicologin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcademicoLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
